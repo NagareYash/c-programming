1bd9ab3a2a06cdfa2df6b1581833d5c3c798baf5
NagareYash <yashnagare10@gmail.com>
2026-04-08 14:23:50 -0400

    Add debug macro implementation to facilitate debugging output

---
+++ src/debug_macro.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define DEBUG   /* Comment this line out to disable debug mode */
+
+int main() {
+    int x = 10;
+
+#ifdef DEBUG
+    printf("DEBUG mode: ON\n");
+    printf("DEBUG: Initial value of x = %d\n", x);
+#else
+    printf("DEBUG mode: OFF\n");
+#endif
+
+    x++;
+
+#ifdef DEBUG
+    printf("DEBUG: After increment, x = %d\n", x);
+#endif
+
+    printf("Final value: %d\n", x);
+    return 0;
+}`
